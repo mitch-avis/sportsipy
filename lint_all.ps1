faae980a16056b12017f522cec490ccfc56972fb
Mitch Avis <mitchavis@gmail.com>
2025-09-09 18:10:24 -0600

    Enhance linting script to exclude tests and docs from isort, black, flake8, and pylint

--- lint_all.ps1
+++ lint_all.ps1
@@ -1,11 +1,11 @@
 Write-Host "Sorting imports with isort..."
-& isort . --skip venv
+& isort . --skip venv --skip tests --skip docs
 
 Write-Host "Reformatting with black..."
-& black . --exclude 'venv/'
+& black . --exclude 'venv/|tests/|docs/' 
 
 Write-Host "Linting with flake8..."
-& flake8 . --exclude=venv
+& flake8 . --exclude=venv,tests,docs
 
 Write-Host "Linting with pylint..."
-& pylint . --ignore=venv
+& pylint . --ignore=venv,tests,docs
