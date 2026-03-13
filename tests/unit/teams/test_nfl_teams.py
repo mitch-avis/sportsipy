c486e89945295c09b3ab364eafaf3abd5ad492c2
Mitch Avis <mitchavis@gmail.com>
2026-03-12 22:49:27 -0600

    test(unit): update unit expectations for parser behavior changes

--- tests/unit/teams/test_nfl_teams.py
+++ tests/unit/teams/test_nfl_teams.py
@@ -1,3 +1,5 @@
+"""Provide utilities for test nfl teams."""
+
 from flexmock import flexmock
 
 from sportsipy.nfl.schedule import Schedule
@@ -5,7 +7,10 @@ from sportsipy.nfl.teams import Team
 
 
 class TestNFLTeams:
+    """Represent TestNFLTeams."""
+
     def test_nfl_schedule_returns_schedule(self, *args, **kwargs):
+        """Return test nfl schedule returns schedule."""
         flexmock(Team).should_receive("_parse_team_data").and_return(None)
         flexmock(Schedule).should_receive("_pull_schedule").and_return(None)
 
