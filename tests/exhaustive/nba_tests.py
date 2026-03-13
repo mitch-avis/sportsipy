6ea47fdcc535ccab63be249a57a4742dbea5fbe7
Mitch Avis <mitchavis@gmail.com>
2026-03-12 22:49:26 -0600

    test(exhaustive): resolve pyright typing in exhaustive scripts

--- tests/exhaustive/nba_tests.py
+++ tests/exhaustive/nba_tests.py
@@ -1,16 +1,20 @@
+"""Provide utilities for nba tests."""
+
 import os
 import sys
+from typing import Any
 
 sys.path.append(os.path.dirname(os.path.dirname(sys.path[0])))
 from sportsipy.nba.teams import Teams
 
 for team in Teams():
-    print(team.name)
-    for player in team.roster.players:
+    team_obj: Any = team
+    print(team_obj.name)
+    for player in team_obj.roster.players:
         try:
             print(player.name)
         except UnicodeEncodeError:
             print(player.name.encode("utf-8"))
-    for game in team.schedule:
+    for game in team_obj.schedule:
         print(game.dataframe)
         print(game.dataframe_extended)
