06618e0706402aea962424776214eaa8afe76dae
Mitch Avis <mitchavis@gmail.com>
2026-03-12 22:49:25 -0600

    fix(parsing): improve player parsing compatibility with fixtures

--- sportsipy/ncaaf/player.py
+++ sportsipy/ncaaf/player.py
@@ -1,9 +1,11 @@
+"""Provide utilities for player."""
+
 from functools import wraps
 
-from pyquery import PyQuery as pq
+from pyquery import PyQuery
 
-from .. import utils
-from .constants import BOXSCORE_RETRY, PLAYER_SCHEME
+from sportsipy import utils
+from sportsipy.ncaaf.constants import BOXSCORE_RETRY, PLAYER_SCHEME
 
 
 def _int_property_decorator(func):
@@ -37,8 +39,7 @@ def _float_property_decorator(func):
 
 
 class AbstractPlayer:
-    """
-    Get player information and stats for all seasons.
+    """Get player information and stats for all seasons.
 
     Given a player ID, such as 'david-blough-1' for David Blough, capture all
     relevant stats and information like name, team, height/weight, career
@@ -66,9 +67,11 @@ class AbstractPlayer:
         A string representation of the player's HTML data from the Boxscore
         page. If the player appears in multiple tables, all of their
         information will appear in one single string concatenated together.
+
     """
 
     def __init__(self, player_id, player_name, player_data):
+        """Initialize the class instance."""
         self._player_id = player_id
         self._name = player_name
         # Passing-specific stats
@@ -124,8 +127,7 @@ class AbstractPlayer:
         self._parse_player_data(player_data)
 
     def _parse_value(self, stats, field):
-        """
-        Pull the specified value from the HTML contents.
+        """Pull the specified value from the HTML contents.
 
         Given a field, find the corresponding HTML tag for that field and parse
         its value before returning the value as a string. A couple fields, such
@@ -144,6 +146,7 @@ class AbstractPlayer:
         -------
         string
             Returns the desired value as a string.
+
         """
         value = utils.parse_field(PLAYER_SCHEME, stats, field)
         if not value and field in BOXSCORE_RETRY:
@@ -151,8 +154,7 @@ class AbstractPlayer:
         return value
 
     def _parse_player_data(self, player_data):
-        """
-        Parse all player information and set attributes.
+        """Parse all player information and set attributes.
 
         Iterate through each class attribute to parse the data from the HTML
         page and set the attribute value with the result.
@@ -166,6 +168,7 @@ class AbstractPlayer:
             class is inherited from the ``BoxscorePlayer`` class, player_data
             will be a string representing the player's game statistics in HTML
             format.
+
         """
         for field in self.__dict__:
             short_field = str(field)[1:]
@@ -182,87 +185,79 @@ class AbstractPlayer:
             field_stats = []
             if isinstance(player_data, dict):
                 for data in player_data.values():
-                    stats = pq(data["data"])
+                    stats = PyQuery(data["data"])
                     value = self._parse_value(stats, short_field)
                     field_stats.append(value)
             else:
-                stats = pq(player_data)
+                stats = PyQuery(player_data)
                 value = self._parse_value(stats, short_field)
                 field_stats.append(value)
             setattr(self, field, field_stats)
 
     @property
     def player_id(self):
-        """
-        Returns a ``string`` of the player's ID on sports-reference, such as
+        """Return a ``string`` of the player's ID on sports-reference, such as.
+
         'david-blough-1' for David Blough.
         """
         return self._player_id
 
     @property
     def name(self):
-        """
-        Returns a ``string`` of the player's name, such as 'David Blough'.
-        """
+        """Return a ``string`` of the player's name, such as 'David Blough'."""
         return self._name
 
     @_int_property_decorator
     def completed_passes(self):
-        """
-        Returns an ``int`` of the number of completed passes the player threw.
-        """
+        """Return an ``int`` of the number of completed passes the player threw."""
         return self._completed_passes
 
     @_int_property_decorator
     def pass_attempts(self):
-        """
-        Returns an ``int`` of the number of passes the player attempted.
-        """
+        """Return an ``int`` of the number of passes the player attempted."""
         return self._pass_attempts
 
     @_float_property_decorator
     def passing_completion(self):
-        """
-        Returns a ``float`` of the percentage of passes that were caught by a
+        """Return a ``float`` of the percentage of passes that were caught by a.
+
         receiver. Percentage ranges from 0-100.
         """
         return self._passing_completion
 
     @_int_property_decorator
     def passing_yards(self):
-        """
-        Returns an ``int`` of the total number of yards the player gained from
+        """Return an ``int`` of the total number of yards the player gained from.
+
         passing the ball.
         """
         return self._passing_yards
 
     @_int_property_decorator
     def passing_touchdowns(self):
-        """
-        Returns an ``int`` of the number of touchdowns passes the player has
+        """Return an ``int`` of the number of touchdowns passes the player has.
+
         thrown.
         """
         return self._passing_touchdowns
 
     @_int_property_decorator
     def interceptions_thrown(self):
-        """
-        Returns an ``int`` of the number of interceptions the player has
+        """Return an ``int`` of the number of interceptions the player has.
+
         thrown.
         """
         return self._interceptions_thrown
 
     @_float_property_decorator
     def passing_yards_per_attempt(self):
-        """
-        Returns a ``float`` of the number of yards gained per passing attempt.
-        """
+        """Return a ``float`` of the number of yards gained per passing attempt."""
         return self._passing_yards_per_attempt
 
     @_float_property_decorator
     def adjusted_yards_per_attempt(self):
-        """
-        Returns a ``float`` of the adjusted number of yards gained per passing
+        """Return a ``float`` of the adjusted number of yards gained per passing.
+
         attempt, equal to (yards + 20 * pass_touchdowns - 45 * interceptions) /
         pass_attempts.
         """
@@ -270,269 +265,247 @@ class AbstractPlayer:
 
     @_float_property_decorator
     def quarterback_rating(self):
-        """
-        Returns a ``float`` of the player's quarterback rating.
-        """
+        """Return a ``float`` of the player's quarterback rating."""
         return self._quarterback_rating
 
     @_int_property_decorator
     def rush_attempts(self):
-        """
-        Returns an ``int`` of the number of rushing plays the player attempted.
-        """
+        """Return an ``int`` of the number of rushing plays the player attempted."""
         return self._rush_attempts
 
     @_int_property_decorator
     def rush_yards(self):
-        """
-        Returns an ``int`` of the number of rushing yards the player gained.
-        """
+        """Return an ``int`` of the number of rushing yards the player gained."""
         return self._rush_yards
 
     @_float_property_decorator
     def rush_yards_per_attempt(self):
-        """
-        Returns a ``float`` of the average number of yards gained per rushing
+        """Return a ``float`` of the average number of yards gained per rushing.
+
         attempt.
         """
         return self._rush_yards_per_attempt
 
     @_int_property_decorator
     def rush_touchdowns(self):
-        """
-        Returns an ``int`` of the number of rushing touchdowns the player
+        """Return an ``int`` of the number of rushing touchdowns the player.
+
         scored.
         """
         return self._rush_touchdowns
 
     @_int_property_decorator
     def receptions(self):
-        """
-        Returns an ``int`` of the number of receptions the player made.
-        """
+        """Return an ``int`` of the number of receptions the player made."""
         return self._receptions
 
     @_int_property_decorator
     def receiving_yards(self):
-        """
-        Returns an ``int`` of the number of receiving yards the player gained.
-        """
+        """Return an ``int`` of the number of receiving yards the player gained."""
         return self._receiving_yards
 
     @_float_property_decorator
     def receiving_yards_per_reception(self):
-        """
-        Returns a ``float`` of the average number of yards the player gained
+        """Return a ``float`` of the average number of yards the player gained.
+
         per reception.
         """
         return self._receiving_yards_per_reception
 
     @_int_property_decorator
     def receiving_touchdowns(self):
-        """
-        Returns an ``int`` of the number of touchdowns the player scored after
+        """Return an ``int`` of the number of touchdowns the player scored after.
+
         receiving a pass.
         """
         return self._receiving_touchdowns
 
     @_int_property_decorator
     def plays_from_scrimmage(self):
-        """
-        Returns an ``int`` of the combined number of rushing attempts and
+        """Return an ``int`` of the combined number of rushing attempts and.
+
         receptions the player had.
         """
         return self._plays_from_scrimmage
 
     @_int_property_decorator
     def yards_from_scrimmage(self):
-        """
-        Returns an ``int`` of the total number of yards gained from scrimmage
+        """Return an ``int`` of the total number of yards gained from scrimmage.
+
         for both rushing and receiving.
         """
         return self._yards_from_scrimmage
 
     @_float_property_decorator
     def yards_from_scrimmage_per_play(self):
-        """
-        Returns a ``float`` of the average number of yards gained per rushing
+        """Return a ``float`` of the average number of yards gained per rushing.
+
         attempt and/or reception.
         """
         return self._yards_from_scrimmage_per_play
 
     @_int_property_decorator
     def rushing_and_receiving_touchdowns(self):
-        """
-        Returns an ``int`` of the combined number of rushing and receiving
+        """Return an ``int`` of the combined number of rushing and receiving.
+
         touchdowns the player scored.
         """
         return self._rushing_and_receiving_touchdowns
 
     @_int_property_decorator
     def solo_tackles(self):
-        """
-        Returns an ``int`` of the number of tackles the player made by himself.
-        """
+        """Return an ``int`` of the number of tackles the player made by himself."""
         return self._solo_tackles
 
     @_int_property_decorator
     def assists_on_tackles(self):
-        """
-        Returns an ``int`` of the number of assists the player made on tackles.
-        """
+        """Return an ``int`` of the number of assists the player made on tackles."""
         return self._assists_on_tackles
 
     @_int_property_decorator
     def total_tackles(self):
-        """
-        Returns an ``int`` of the number of tackles the player made.
-        """
+        """Return an ``int`` of the number of tackles the player made."""
         return self._total_tackles
 
     @_float_property_decorator
     def tackles_for_loss(self):
-        """
-        Returns a ``float`` of the number of tackles for a loss the player
+        """Return a ``float`` of the number of tackles for a loss the player.
+
         made.
         """
         return self._tackles_for_loss
 
     @_float_property_decorator
     def sacks(self):
-        """
-        Returns a ``float`` of the number of times the player sacked a
+        """Return a ``float`` of the number of times the player sacked a.
+
         quarterback.
         """
         return self._sacks
 
     @_int_property_decorator
     def interceptions(self):
-        """
-        Returns an ``int`` of the number of times the player intercepted a
+        """Return an ``int`` of the number of times the player intercepted a.
+
         pass.
         """
         return self._interceptions
 
     @_int_property_decorator
     def yards_returned_from_interceptions(self):
-        """
-        Returns an ``int`` of the number of yards the player returned after
+        """Return an ``int`` of the number of yards the player returned after.
+
         intercepting a pass.
         """
         return self._yards_returned_from_interceptions
 
     @_float_property_decorator
     def yards_returned_per_interception(self):
-        """
-        Returns a ``float`` of the average number of yards the player returns
+        """Return a ``float`` of the average number of yards the player returns.
+
         after intercepting a pass.
         """
         return self._yards_returned_per_interception
 
     @_int_property_decorator
     def interceptions_returned_for_touchdown(self):
-        """
-        Returns an ``int`` of the number of touchdowns the player has scored
+        """Return an ``int`` of the number of touchdowns the player has scored.
+
         after intercepting a pass. Commonly referred to as a 'Pick-6'.
         """
         return self._interceptions_returned_for_touchdown
 
     @_int_property_decorator
     def passes_defended(self):
-        """
-        Returns an ``int`` of the number of passes the player has defended as a
+        """Return an ``int`` of the number of passes the player has defended as a.
+
         defensive player.
         """
         return self._passes_defended
 
     @_int_property_decorator
     def fumbles_recovered(self):
-        """
-        Returns an ``int`` of the number of fumbles the player has recovered.
-        """
+        """Return an ``int`` of the number of fumbles the player has recovered."""
         return self._fumbles_recovered
 
     @_int_property_decorator
     def yards_recovered_from_fumble(self):
-        """
-        Returns an ``int`` of the number of yards the player gained after
+        """Return an ``int`` of the number of yards the player gained after.
+
         recovering a fumble.
         """
         return self._yards_recovered_from_fumble
 
     @_int_property_decorator
     def fumbles_recovered_for_touchdown(self):
-        """
-        Returns an ``int`` of the number of touchdowns the player has scored
+        """Return an ``int`` of the number of touchdowns the player has scored.
+
         after recovering a fumble.
         """
         return self._fumbles_recovered_for_touchdown
 
     @_int_property_decorator
     def fumbles_forced(self):
-        """
-        Returns an ``int`` of the number of times the player forced a fumble.
-        """
+        """Return an ``int`` of the number of times the player forced a fumble."""
         return self._fumbles_forced
 
     @_int_property_decorator
     def punt_return_touchdowns(self):
-        """
-        Returns an ``int`` of the number of punts the player returned for a
+        """Return an ``int`` of the number of punts the player returned for a.
+
         touchdown.
         """
         return self._punt_return_touchdowns
 
     @_int_property_decorator
     def kickoff_return_touchdowns(self):
-        """
-        Returns an ``int`` of the number of kickoffs the player returned for a
+        """Return an ``int`` of the number of kickoffs the player returned for a.
+
         touchdown.
         """
         return self._kickoff_return_touchdowns
 
     @_int_property_decorator
     def extra_points_made(self):
-        """
-        Returns an ``int`` of the number of extra points the player made.
-        """
+        """Return an ``int`` of the number of extra points the player made."""
         return self._extra_points_made
 
     @_int_property_decorator
     def extra_points_attempted(self):
-        """
-        Returns an ``int`` of the total number of extra points the player
+        """Return an ``int`` of the total number of extra points the player.
+
         attempted.
         """
         return self._extra_points_attempted
 
     @_float_property_decorator
     def extra_point_percentage(self):
-        """
-        Returns a ``float`` of the percentage (0.0 to 100.0) of extra points
+        """Return a ``float`` of the percentage (0.0 to 100.0) of extra points.
+
         the player made.
         """
         return self._extra_point_percentage
 
     @_int_property_decorator
     def field_goals_made(self):
-        """
-        Returns an ``int`` of the total number of field goals the player made
+        """Return an ``int`` of the total number of field goals the player made.
+
         from any distance.
         """
         return self._field_goals_made
 
     @_int_property_decorator
     def field_goals_attempted(self):
-        """
-        Returns an ``int`` of the total number of field goals the player
+        """Return an ``int`` of the total number of field goals the player.
+
         attempted from any distance.
         """
         return self._field_goals_attempted
 
     @_float_property_decorator
     def field_goal_percentage(self):
-        """
-        Returns a ``float`` of the percentage (0.0 to 100.0) of field goals
+        """Return a ``float`` of the percentage (0.0 to 100.0) of field goals.
+
         the player made from any distance.
         """
         return self._field_goal_percentage
