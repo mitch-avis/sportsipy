fee154717c4224845b019cc9d61eb963005d0d78
Mitch Avis <mitchavis@gmail.com>
2026-03-12 22:49:25 -0600

    fix(parsing): refresh selector constants for fixture compatibility

--- sportsipy/ncaaf/constants.py
+++ sportsipy/ncaaf/constants.py
@@ -1,3 +1,5 @@
+"""Provide utilities for constants."""
+
 PARSING_SCHEME = {
     "conference_losses": 'td[data-stat="losses_conf"]:first',
     "conference_win_percentage": 'td[data-stat="win_loss_pct_conf"]:first',
